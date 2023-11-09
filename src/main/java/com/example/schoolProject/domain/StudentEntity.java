d7038fcbaf415ee93bf367ee230de4d3a1cb2550
Damian <damian@example.com>
2023-11-09 15:49:23 +0100

    Added body to student classes

--- src/main/java/com/example/schoolProject/domain/StudentEntity.java
+++ src/main/java/com/example/schoolProject/domain/StudentEntity.java
@@ -1,4 +1,23 @@
 package com.example.schoolProject.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "student")
+@Data
 public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String surname;
+    private List<Double> scores;
+
+    public StudentEntity(){
+        scores = new ArrayList<>();
+    }
 }
--- src/main/java/com/example/schoolProject/service/StudentServiceImpl.java
+++ src/main/java/com/example/schoolProject/service/StudentServiceImpl.java
@@ -1,4 +1,39 @@
 package com.example.schoolProject.service;
 
-public class StudentServiceImpl implements StudentService{
+import com.example.schoolProject.domain.StudentEntity;
+import com.example.schoolProject.repository.StudentRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public StudentEntity getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+    @Override
+    public StudentEntity getStudentByName(String name) {
+        return studentRepository.findByName(name);
+    }
+    @Override
+    public List<StudentEntity> getAllStudents() {
+        return studentRepository.findAll();
+    }
+    @Override
+    public boolean exists(String name) {
+        if (studentRepository.findByName(name)!=null) {
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public StudentEntity save(StudentEntity student) {
+        return studentRepository.save(student);
+    }
 }
